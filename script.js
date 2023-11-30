be59d55639ae629bf02bee29e8e5c871c28ab8a1
Ameerhwais <144728369+Ameerhwais@users.noreply.github.com>
2023-11-30 04:49:16 +0000

    assignment 4

--- script.js
+++ script.js
@@ -41,53 +41,60 @@ function inititialise() {
 // Inits & Event Listeners
 //
 inititialise();
+let todos = [];
 
-if (todos.length === 0) {
-  emptyState.style.display = 'block';
-} else {
-  emptyState.style.display = 'none';
-  todos.forEach(todo => {
-      const listItem = document.createElement('li');
-      listItem.className = `todo-item ${todo.completed ? 'completed' : ''}`;
-      listItem.innerHTML = `
-          <span>${todo.text}</span>
-          <button onclick="toggleTodoStatus(${todo.id})">Done</button>
-          <button onclick="deleteTodo(${todo.id})">Delete</button>
-      `;
-      todoList.appendChild(listItem);
-  });
+function renderTodos() {
+    const todoList = document.getElementById('todoList');
+    const emptyState = document.getElementById('emptyState');
+
+    todoList.innerHTML = '';
+
+    if (todos.length === 0) {
+        emptyState.style.display = 'block';
+    } else {
+        emptyState.style.display = 'none';
+        todos.forEach(todo => {
+            const listItem = document.createElement('li');
+            listItem.className = `todo-item ${todo.completed ? 'completed' : ''}`;
+            listItem.innerHTML = `
+                <span>${todo.text}</span>
+                <button onclick="toggleTodoStatus(${todo.id})">Done</button>
+                <button onclick="deleteTodo(${todo.id})">Delete</button>
+            `;
+            todoList.appendChild(listItem);
+        });
+    }
 }
-
 
 function addTodo() {
-const todoInput = document.getElementById('todoInput');
-const todoText = todoInput.value.trim();
+    const todoInput = document.getElementById('todoInput');
+    const todoText = todoInput.value.trim();
 
-if (todoText !== '') {
-  const newTodo = {
-      id: todos.length + 1,
-      text: todoText,
-      completed: false,
-  };
+    if (todoText !== '') {
+        const newTodo = {
+            id: todos.length + 1,
+            text: todoText,
+            completed: false,
+        };
 
-  todos.push(newTodo);
-  todoInput.value = '';
-  renderTodos();
-}
+        todos.push(newTodo);
+        todoInput.value = '';
+        renderTodos();
+    }
 }
 
 function toggleTodoStatus(todoId) {
-const todoIndex = todos.findIndex(todo => todo.id === todoId);
+    const todoIndex = todos.findIndex(todo => todo.id === todoId);
 
-if (todoIndex !== -1) {
-  todos[todoIndex].completed = !todos[todoIndex].completed;
-  renderTodos();
-}
+    if (todoIndex !== -1) {
+        todos[todoIndex].completed = !todos[todoIndex].completed;
+        renderTodos();
+    }
 }
 
 function deleteTodo(todoId) {
-todos = todos.filter(todo => todo.id !== todoId);
-renderTodos();
+    todos = todos.filter(todo => todo.id !== todoId);
+    renderTodos();
 }
 
 // Initial rendering
